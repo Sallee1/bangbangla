26faad3105885ae2e55a138ad99386594957dd70
Sallee_admin <885331635@qq.com>
2022-07-06 16:17:21 +0800

    新建项目

---
+++ src/main/java/com/sallee/bangbangla/BangbanglaApplication.java
@@ -0,0 +1,13 @@
+package com.sallee.bangbangla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangbanglaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BangbanglaApplication.class, args);
+  }
+
+}
