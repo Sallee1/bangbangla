3f4e06ca7026e9a0501c782e0c9fcd975d864bfd
DongyiMing00 <1193623892@qq.com>
2022-07-08 11:47:42 +0800

    Userinter后端

--- src/main/java/com/sallee/bangbangla/service/impl/UserInterServerImpl.java
+++ src/main/java/com/sallee/bangbangla/service/impl/UserInterServerImpl.java
@@ -1,12 +1,17 @@
 package com.sallee.bangbangla.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sallee.bangbangla.mapper.ChatMapper;
 import com.sallee.bangbangla.mapper.OrderHistoryMapper;
 import com.sallee.bangbangla.mapper.ReportMapper;
 import com.sallee.bangbangla.mapper.UserMapper;
+import com.sallee.bangbangla.pojo.DAO.ChatDAO;
+import com.sallee.bangbangla.pojo.DAO.ItemHistoryDAO;
+import com.sallee.bangbangla.pojo.DAO.ReportDAO;
 import com.sallee.bangbangla.pojo.DTO.ChatDTO;
 import com.sallee.bangbangla.pojo.DTO.CommitDTO;
 import com.sallee.bangbangla.pojo.DTO.ReportDTO;
+import com.sallee.bangbangla.pojo.Enum;
 import com.sallee.bangbangla.service.UserInterServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,16 +29,48 @@ public class UserInterServerImpl implements UserInterServer {
 
 	@Override
 	public boolean addChat(ChatDTO chatDTO) {
-		return false;
+
+		ChatDAO addChat = new ChatDAO();
+		addChat.setData(chatDTO.getData());
+		addChat.setReceiverId(chatDTO.getReceiverId());
+		addChat.setSenderId(chatDTO.getSenderId());
+		addChat.setType(Enum.ChatType.valueOf(chatDTO.getType()).ordinal());
+
+		int addRow = chatMapper.insert(addChat);
+
+		return addRow>=1?true:false;
 	}
 
 	@Override
 	public boolean addReport(ReportDTO reportDTO) {
-		return false;
+
+		ReportDAO reportDAO = new ReportDAO();
+		reportDAO.setBeReportId(reportDTO.getBeReportId());
+		reportDAO.setReportId(reportDTO.getReportId());
+		reportDAO.setItemId(reportDTO.getItemId());
+		reportDAO.setResult(reportDTO.getResult());
+
+		int addRow = reportMapper.insert(reportDAO);
+
+		return addRow>=1?true:false;
 	}
 
 	@Override
 	public boolean addCommit(CommitDTO commitDTO) {
-		return false;
+		QueryWrapper queryWrapper = new QueryWrapper();
+		queryWrapper.eq("id",commitDTO.getItemId());
+
+		ItemHistoryDAO itemHistoryDAO = orderHistoryMapper.selectOne(queryWrapper);
+
+		if(itemHistoryDAO==null){
+			throw new RuntimeException("没有查到Id");
+		}
+		else{
+			ItemHistoryDAO addCom = new ItemHistoryDAO();
+			addCom.setCommit(commitDTO.getText());
+			addCom.setCredit(commitDTO.getCredit());
+			return true;
+		}
+
 	}
 }
